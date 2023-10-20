1481605fbb2b63c504ba60d2addfa5022db2ab5d
PREDATOR <piscesscorpio11819@gmail.com>
2023-10-20 14:15:19 +0530

    d

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.get('/', function (req, res) { 
-  res.send('Api is working!');
+  res.json({msg: process.env.PORT});
 }) 
 
 mongoose
